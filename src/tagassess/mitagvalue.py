dc8d49b7e5281ff7d3f22907f512544c17728690
flaviovdf <flaviovdf@gmail.com>
2011-03-28 12:58:48 -0700

    Minor change in comment

--- src/tagassess/mitagvalue.py
+++ src/tagassess/mitagvalue.py
@@ -5,8 +5,8 @@ from __future__ import print_function, division
 MUTUAL INFORMATION TAG VALUE
 ============================
 
-Implementation of the tag assessment method described on:
-TODO: Insert url
+Implementation of the tag assessment method described on: 
+http://dx.doi.org/10.1109/SocialCom.2010.69
 '''
 
 import numpy as np
