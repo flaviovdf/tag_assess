ada162755477b62aa344f1906ec3d2979efdc37d
flaviovdf <flaviovdf@gmail.com>
2011-07-15 17:34:49 -0700

    Script for computing global graph with global tag values

--- src/scripts/GlobalGraphAndValues.py
+++ src/scripts/GlobalGraphAndValues.py
@@ -1,14 +1,12 @@
 # -*- coding: utf8
 '''
-Computes tag files and value files filtering out
-half of the annotations per user.
+Computes a tag tag graph and the global value for
+those tags
 '''
 from __future__ import division, print_function
 
 __authors__ = ['Flavio Figueiredo - flaviovdf <at> gmail <dot-no-spam> com']
 __date__ = '26/05/2011'
-
-from itertools import ifilter
 
 from tagassess import index_creator
 from tagassess import smooth
@@ -27,7 +25,7 @@ import sys
 import traceback
 import tempfile
 
-def create_graph(annotation_it, user_folder):
+def create_graph(annotation_it, out_folder):
     ntags, nsinks, iedges = \
      graph.iedge_from_annotations(annotation_it, 1,
                                   False)
@@ -41,7 +39,7 @@ def create_graph(annotation_it, user_folder):
             n_edges += 1
 
     with io.open(tmp_fname) as tmp:
-        out_graph = os.path.join(user_folder, 'navi.graph')
+        out_graph = os.path.join(out_folder, 'navi.graph')
         with io.open(out_graph, 'w') as out:
             out.write(u'#Nodes:  %d\n'%n_nodes)
             out.write(u'#Edges:  %d\n'%n_edges)
@@ -49,64 +47,42 @@ def create_graph(annotation_it, user_folder):
             for line in tmp:
                 out.write(line)
 
-def compute_tag_values(annotation_it, user, user_folder):
+def compute_tag_values(annotation_it, idx, out_folder):
     smooth_func = smooth.bayes
     lambda_ = 0.25
     est = SmoothEstimator(smooth_func, lambda_, annotation_it, cache = False)
     recc = ProbabilityReccomender(est)
     value_calc = value_calculator.ValueCalculator(est, recc)
     
-    itag_value = value_calc.itag_value_ucontext(user)
-    with io.open(os.path.join(user_folder, 'tag.values'), 'w') as values:
+    itag_value = value_calc.itag_value_gcontext()
+    with io.open(os.path.join(out_folder, 'tag.values'), 'w') as values:
         for tag_val, tag in itag_value:
-            values.write(u'%d %.5f\n' % (tag, tag_val))
+            mean_prob = est.vect_prob_item([item for item in idx[tag]]).mean()
+            final_val = tag_val * mean_prob
+            values.write(u'%d %.5f\n' % (tag, final_val))
               
 def real_main(database, table, out_folder):
     
     with AnnotReader(database) as reader:
         reader.change_table(table) 
-        idx = index_creator.create_occurrence_index(reader.iterate(),
-                                                     'user', 'item')
         
-        filt = lambda u: len(idx[u]) >= 10
-        for user in ifilter(filt, idx.iterkeys()):
-            items = [item for item in idx[user]]
-            half = len(items) // 2
+        #Create Graph
+        create_graph(reader.iterate(), out_folder)
+      
+        #Compute tag value
+        idx = index_creator.create_occurrence_index(reader.iterate(), 
+                                                    'tag', 'item')
+        compute_tag_values(reader.iterate(), idx, out_folder)
+        
+        #Compute popularity
+        tag_pop = collections.defaultdict(int)
+        for annotation in reader.iterate():
+            tag = annotation['tag']
+            tag_pop[tag] += 1
             
-            relevant = items[:half]
-            annotated = items[half:]
-            
-            query = {'$or' : [
-                              { 'user':{'$ne'  : user} }, 
-                              { 'item':{'$nin' : annotated} }
-                             ]
-                    }
-            
-            fname = 'user_%d' % user
-            user_folder = os.path.join(out_folder, fname)
-            os.mkdir(user_folder)
-            
-            #Initial information
-            with io.open(os.path.join(user_folder, 'info'), 'w') as info:
-                info.write(u'#UID: %d\n' %user)
-                info.write(u'#Relevant  items: %s\n' %str(relevant))
-                info.write(u'#Annotated items: %s\n' %str(annotated))
-            
-            #Create Graph
-            create_graph(reader.iterate(query = query), user, user_folder)
-          
-            #Compute tag value
-            compute_tag_values(reader.iterate(query = query), user, user_folder)
-            
-            #Compute popularity
-            tag_pop = collections.defaultdict(int)
-            for annotation in reader.iterate(query = query):
-                tag = annotation['tag']
-                tag_pop[tag] += 1
-                
-            with io.open(os.path.join(user_folder, 'tag.pop'), 'w') as pops:
-                for tag in tag_pop:
-                    pops.write(u'%d %d\n' % (tag, tag_pop[tag]))
+        with io.open(os.path.join(out_folder, 'tag.pop'), 'w') as pops:
+            for tag in tag_pop:
+                pops.write(u'%d %d\n' % (tag, tag_pop[tag]))
                      
 def create_parser(prog_name):
     parser = argparse.ArgumentParser(prog=prog_name,
